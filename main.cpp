11bf42ce6af31516f31471ea0b620e3c2dc2aacd
elias al3x <cod3er4@gmail.com>
2020-04-07 16:46:12 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // importa uma biblioteca para o projeto { input + output + stream }
+
+int main()
+{
+  std::cout << "Hello, world!\n";
+  return 0;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# C++ Initial
+
+## Objetivos
+
+- Objetivos
+  - [x] Incluir biblioteca
+        `#include <iostream>`
